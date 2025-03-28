9b1fe06b8688be5adb503c2370ee26922042fff9
programadormovel <programadormovel@gmail.com>
2025-03-28 09:57:36 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,31 @@ function App() {
 
   // var nome = "Neymar Jr";
 
+  const Formulario = () => {
+    return (
+      <div>
+        <input
+          className="nome"
+          name = "nome"
+          onChange={(e)=>{setNome(e.target.value)}}
+          placeholder="Digite um nome..."
+          type="text" />
+        <button 
+          className="botao"
+          onClick={()=>{alert(nome)}} >
+            CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h3>Pizzaria 2F</h3>
-      <input
-        className="nome"
-        name = "nome"
-        onChange={(e)=>{setNome(e.target.value)}}
-        placeholder="Digite um nome..."
-        type="text" />
-      <button 
-        className="botao"
-        onClick={()=>{alert(nome)}} >
-          CLIQUE AQUI
-      </button>
+      <Formulario />
+      <Formulario />
+      <Formulario />
+
     </div>
   )
 }
