561c96cf1a58a6156beaaa8dea751bb94c68a326
programadormovel <programadormovel@gmail.com>
2025-04-04 09:59:02 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
+import poke from './assets/pokeapi_256.png'
 
 function App() {
 
-  const [nome, setNome] = useState("Memphis")
+  const Formulario = (props) => {
 
-  // var nome = "Neymar Jr";
+    const [nome, setNome] = useState("Memphis")
 
-  const Formulario = () => {
     return (
       <div>
+
         <input
           className="nome"
           name = "nome"
           onChange={(e)=>{setNome(e.target.value)}}
-          placeholder="Digite um nome..."
+          placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
           type="text" />
+
         <button 
           className="botao"
           onClick={()=>{alert(nome)}} >
             CLIQUE AQUI
         </button>
+
       </div>
     )
   }
@@ -27,6 +30,12 @@ function App() {
   return (
     <div>
       <h3>Pizzaria 2F</h3>
+      <p>Texto do parágrafo</p>
+      <img src={poke} style={{width:200, height:100}} />
+      <img src={poke} style={{width:200, height:100}} />
+      <Formulario sombra="Digite seu nome..." />
+      <Formulario sombra="Digite seu e-mail..."/>
+      <Formulario sombra="(99)90000-0000"/>
       <Formulario />
       <Formulario />
       <Formulario />
