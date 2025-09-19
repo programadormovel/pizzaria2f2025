54da5dd1c037e7bb15f889cac7a837f45a6391d8
programadormovel <programadormovel@gmail.com>
2025-09-19 08:18:33 -0300

    lista

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,15 +1,19 @@
 const Produtos = () => {
+    // Objeto com a lista de pizzas
+    const pizzas = [
+        'Pizza de Muçarela', 
+        'Pizza de Calabresa',
+        'Pizza Baiana', 'Pizza Quatro Queijos'
+    ]
+    // Iteração da lista de pizzas
+    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
 
     return (
         <div>
             <ul>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza de Calabreza</li>
-                <li>Pizza Portuguesa</li>
-                <li>Pizza Baiana</li>
+                {listaPizzas}
             </ul>
         </div>
     )
 }
-
 export default Produtos
