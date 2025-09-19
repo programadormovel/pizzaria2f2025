7df326775cdef100b9d04d566a14cd66ac71dd99
programadormovel <programadormovel@gmail.com>
2025-09-19 07:47:43 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Produtos from "./pages/produtos"
+
 const App = () => {
 
 
   return (
     <>
       <h1>Projeto Pizzaria</h1>
+      <Produtos />
     </>
   )
 }
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produtos = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>Pizza de Muçarela</li>
+                <li>Pizza de Calabreza</li>
+                <li>Pizza Portuguesa</li>
+                <li>Pizza Baiana</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Produtos
